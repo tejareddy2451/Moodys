ff286dc74663536e1e2a7bb9714a36bb593ce0d2
tejareddy2451 <128737799+tejareddy2451@users.noreply.github.com>
2023-06-09 21:31:57 +0530

    Reformatting

--- all tables.py
+++ all tables.py
@@ -17,15 +17,13 @@ def build_spark_options(query=None, read=False, write=False, get_uname=None):
     else:
         secret_data = json.loads(base64.b64decode(secret_value[projectConstants.SEC_BIN]))
 
-    spark_options = {}
-    spark_options[projectConstants.USER] = secret_data[projectConstants.USERNAME]
-    spark_options[projectConstants.PWD] = secret_data[projectConstants.PWD]
-    spark_options[projectConstants.HOST] = secret_data[projectConstants.HOST]
-    spark_options[projectConstants.PORT] = secret_data[projectConstants.PORT]
-    spark_options[projectConstants.DBNAME] = secret_data[projectConstants.DBNAME]
+    spark_options = {projectConstants.USER: secret_data[projectConstants.USERNAME],
+                     projectConstants.PWD: secret_data[projectConstants.PWD],
+                     projectConstants.HOST: secret_data[projectConstants.HOST],
+                     projectConstants.PORT: secret_data[projectConstants.PORT],
+                     projectConstants.DBNAME: secret_data[projectConstants.DBNAME]}
 
-    spark_options[
-        "url"] = f"""jdbc:postgresql://{spark_options['host']}:{spark_options['port']}/{spark_options['dbname']}"""
+    spark_options["url"] = f"""jdbc:postgresql://{spark_options['host']}:{spark_options['port']}/{spark_options['dbname']}"""
 
     if get_uname:
         return spark_options["user"]
@@ -68,11 +66,10 @@ def build_postgres_options():
     else:
         secret_data = json.loads(base64.b64decode(secret_value[projectConstants.SEC_BIN]))
 
-    postgres_options = {}
-    postgres_options[projectConstants.USER] = secret_data[projectConstants.USERNAME]
-    postgres_options[projectConstants.PWD] = secret_data[projectConstants.PWD]
-    postgres_options[projectConstants.HOST] = secret_data[projectConstants.HOST]
-    postgres_options[projectConstants.DBNAME] = secret_data[projectConstants.DBNAME]
+    postgres_options = {projectConstants.USER: secret_data[projectConstants.USERNAME],
+                        projectConstants.PWD: secret_data[projectConstants.PWD],
+                        projectConstants.HOST: secret_data[projectConstants.HOST],
+                        projectConstants.DBNAME: secret_data[projectConstants.DBNAME]}
     return postgres_options
 
 
@@ -90,7 +87,7 @@ def bulk_upsert(upsert_tables_list, read_df_dict):
 
 def main():
     print(f"Execution - Started")
-    table = ''
+
     meth_query = f"""select distinct
        c.meth_id,
        trim(Methodology_Name) nm,
@@ -103,11 +100,12 @@ where Methodology_Document_Version_Number = (select max(Methodology_Document_Ver
                                             where trim(b.Methodology_Name) = trim(a.Methodology_Name)
                                             )"""
     spark_options = build_spark_options(query=meth_query, read=True, write=False)
+    table = 'meth'
     df = read(table, spark_options)
 
-    print(f"Upsert - Started")
+    print(f"Upsert - Started for {table}")
     meth_upsert_table = [{
-        'target_table': 'meth',
+        'target_table': table,
         'unique_key_list': ['meth_nm'],
         'target_cols_list': ['merg_to_meth_id', 'meth_nm', 'meth_typ_cd']
     }]
@@ -116,15 +114,15 @@ where Methodology_Document_Version_Number = (select max(Methodology_Document_Ver
     # meth_doc
     meth_doc_query = f"""select 
        meth_id,
-	   Publishing_Entity_Code,
-	   Language_Name,
-	   Methodology_Document_Version_Number::int,
-	   Methodology_Document_Type_Code,
-	   trim(Methodology_Document_Publication_Name),
-	   Methodology_Document_CMS_Document_Identifier,
-	   left(Methodology_Document_RMC_Document_Identifier,-2),
+       Publishing_Entity_Code,
+       Language_Name,
+       Methodology_Document_Version_Number::int,
+       Methodology_Document_Type_Code,
+       trim(Methodology_Document_Publication_Name),
+       Methodology_Document_CMS_Document_Identifier,
+       left(Methodology_Document_RMC_Document_Identifier,-2),
        Methodology_Document_Publication_Datetime::timestamp,
-	   Methodology_Document_Modification_DateTime::timestamp,
+       Methodology_Document_Modification_DateTime::timestamp,
        c.meth_doc_Status_cd
 from methodology_stg.stg_meth_doc a,
      methodology.meth b,
@@ -133,11 +131,12 @@ where trim(a.Methodology_Name) = b.meth_nm
   and trim(a.Methodology_Status) = c.meth_doc_Status_desc
   order by meth_id,Methodology_Document_Publication_Datetime"""
     spark_options = build_spark_options(query=meth_doc_query, read=True, write=False)
+    table = 'meth_doc'
     df = read(table, spark_options)
 
-    print(f"Upsert - Started")
+    print(f"Upsert - Started for {table}")
     meth_doc_upsert_table = [{
-        'target_table': 'meth_doc',
+        'target_table': table,
         'unique_key_list': ['cms_doc_id'],
         'target_cols_list': ['meth_id', 'publ_entity_cd', 'lang_iso_alpha_3_cd', 'meth_vrsn_num',
                              'meth_doc_typ_cd', 'publ_titl_text', 'cms_doc_id', 'rmc_doc_id', 'publcn_dtm',
@@ -146,23 +145,25 @@ where trim(a.Methodology_Name) = b.meth_nm
     bulk_upsert(meth_doc_upsert_table, df)
 
     # rfc_doc
-    rfc_doc_query = f"""select 
-       trim(Request_for_Comment_Publication_Name),
-	   Request_for_Comment_Open_Datetime::timestamp,
-	   Request_for_Comment_Close_Datetime::timestamp,
-	   Request_for_Comment_Original_Close_Datetime::timestamp,
-       Request_for_Comment_CMS_Document_Identifier,
-	   Request_for_Comment_RMC_Document_Identifier,
-	   b.rfc_status_cd
-from methodology_stg.stg_rfc_roc a,
-     methodology.lkp_rfc_status b
- where a.Request_for_Comment_Status_Code = b.rfc_status_desc"""
+    rfc_doc_query = f"""
+    select 
+           trim(Request_for_Comment_Publication_Name),
+           Request_for_Comment_Open_Datetime::timestamp,
+           Request_for_Comment_Close_Datetime::timestamp,
+           Request_for_Comment_Original_Close_Datetime::timestamp,
+           Request_for_Comment_CMS_Document_Identifier,
+           Request_for_Comment_RMC_Document_Identifier,
+           b.rfc_status_cd
+    from methodology_stg.stg_rfc_roc a,
+        methodology.lkp_rfc_status b
+    where a.Request_for_Comment_Status_Code = b.rfc_status_desc"""
     spark_options = build_spark_options(query=rfc_doc_query, read=True, write=False)
+    table = 'rfc_doc'
     df = read(table, spark_options)
 
-    print(f"Upsert - Started")
+    print(f"Upsert - Started for {table}")
     rfc_doc_upsert_table = [{
-        'target_table': 'rfc_doc',
+        'target_table': table,
         'unique_key_list': ['rfc_cms_doc_id'],
         'target_cols_list': ['rfc_publcn_name', 'rfc_open_dtm', 'rfc_close_dtm', 'rfc_origin_close_dtm',
                              'rfc_cms_doc_id', 'rfc_rmc_doc_id', 'rfc_status_cd']
@@ -170,28 +171,30 @@ from methodology_stg.stg_rfc_roc a,
     bulk_upsert(rfc_doc_upsert_table, df)
 
     # roc_doc
-    roc_doc_query = f"""select 
-       rfc_id,
-       Result_of_Consultation_Publication_Name,
-	   Result_of_Consultation_CMS_Document_Identifier,
-	   Result_of_Consultation_RMC_Document_Identifier,
-	   Result_of_Consultation_Publication_Date::timestamp,
-	   c.roc_status_cd
-from methodology_stg.stg_rfc_roc a,  
-     methodology.rfc_doc b,
-     methodology.lkp_roc_status c
-where Request_for_Comment_CMS_Document_Identifier = rfc_cms_doc_id	 
-  and Request_for_Comment_Publication_Name = rfc_publcn_name
-  and Request_for_Comment_Open_Datetime::timestamp = rfc_open_dtm
-  and Request_for_Comment_Close_Datetime::timestamp = rfc_close_dtm
-  and Request_for_Comment_Original_Close_Datetime::timestamp = rfc_origin_close_dtm
-  and a.Result_of_Consultation_Status_Code = c.roc_status_desc"""
+    roc_doc_query = f"""
+        select 
+           rfc_id,
+           Result_of_Consultation_Publication_Name,
+           Result_of_Consultation_CMS_Document_Identifier,
+           Result_of_Consultation_RMC_Document_Identifier,
+           Result_of_Consultation_Publication_Date::timestamp,
+            c.roc_status_cd
+        from methodology_stg.stg_rfc_roc a,  
+         methodology.rfc_doc b,
+         methodology.lkp_roc_status c
+        where Request_for_Comment_CMS_Document_Identifier = rfc_cms_doc_id	 
+          and Request_for_Comment_Publication_Name = rfc_publcn_name
+          and Request_for_Comment_Open_Datetime::timestamp = rfc_open_dtm
+          and Request_for_Comment_Close_Datetime::timestamp = rfc_close_dtm
+          and Request_for_Comment_Original_Close_Datetime::timestamp = rfc_origin_close_dtm
+          and a.Result_of_Consultation_Status_Code = c.roc_status_desc"""
     spark_options = build_spark_options(query=roc_doc_query, read=True, write=False)
+    table = 'roc_doc'
     df = read(table, spark_options)
 
-    print(f"Upsert - Started")
+    print(f"Upsert - Started for {table}")
     roc_doc_upsert_table = [{
-        'target_table': 'roc_doc',
+        'target_table': table,
         'unique_key_list': ['roc_cms_doc_id'],
         'target_cols_list': ['rfc_id', 'roc_publcn_nm', 'roc_cms_doc_id', 'roc_rmc_doc_id', 'roc_publcn_dtm',
                              'roc_status_cd']
@@ -213,17 +216,18 @@ where a.batch = d.batch
   and c.meth_doc_typ_cd = d.Methodology_Document_Type_Code
   and c.publ_entity_cd = d.Publishing_Entity_Code"""
     spark_options = build_spark_options(query=meth_rfc_query, read=True, write=False)
+    table = 'meth_rfc'
     df = read(table, spark_options)
 
-    print(f"Upsert - Started")
+    print(f"Upsert - Started for {table}")
     meth_rfc_upsert_table = [{
-        'target_table': 'meth_rfc',
+        'target_table': table,
         'unique_key_list': ['rfc_id', 'meth_doc_id'],
         'target_cols_list': ['rfc_id', 'meth_doc_id']
     }]
     bulk_upsert(meth_rfc_upsert_table, df)
 
-    print(f"Upsert - Completed")
+    print(f"Execution - Completed")
 
 
 if __name__ == '__main__':
